b371969897ce986a5b6f3ddb7a740455b232f510
SAMSON <samsyano1@gmail.com>
2017-07-12 09:47:33 +0100

    Today 12 - 7 - 2017 Adding the spinner

---
+++ app/src/main/java/com/example/samson/pets/EditorActivity.java
@@ -0,0 +1,98 @@
+package com.example.samson.pets;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class EditorActivity extends AppCompatActivity {
+
+    EditText nameText;
+    EditText breedText;
+    EditText weightText;
+
+    Spinner genderSpinner;
+
+    private int mGender = 0;
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+//        return super.onCreateOptionsMenu(menu);
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
+        nameText = (EditText) findViewById(R.id.edit_pet_name);
+        breedText = (EditText) findViewById(R.id.edit_pet_breed);
+        weightText = (EditText) findViewById(R.id.edit_pet_weight);
+
+        genderSpinner = (Spinner) findViewById(R.id.spinner_gender);
+
+        ArrayAdapter genderSpinnerAdapter = ArrayAdapter.createFromResource(this, R.array.array_gender_options, R.layout.support_simple_spinner_dropdown_item);
+        genderSpinnerAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(genderSpinnerAdapter);
+
+        genderSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selection = (String) parent.getItemAtPosition(position);
+
+                if(!TextUtils.isEmpty(selection)){
+                    if(selection.equals(getString(R.string.gender_male))){
+                        mGender = 1;
+                    }else if(selection.equals(getString(R.string.gender_female))){
+                        mGender = 2;
+                    }else{
+                        mGender = 0;
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                mGender = 0;
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_save:
+
+                return true;
+            case R.id.action_delete:
+
+                return true;
+            case R.id.home:
+                Intent intent = getParentActivityIntent();
+                if(NavUtils.shouldUpRecreateTask(this, intent)){
+                    TaskStackBuilder.create(getApplicationContext())
+                            .addNextIntentWithParentStack(intent)
+                            .startActivities();
+                }else{
+                    NavUtils.navigateUpTo(this, intent);
+                }
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/example/samson/pets/MainActivity.java
+++ app/src/main/java/com/example/samson/pets/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.samson.pets;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton floatingActionButton;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+//        return super.onCreateOptionsMenu(menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionButton = (FloatingActionButton)findViewById(R.id.add_pet);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_insert_dummy_data:
+                Intent intent = new Intent(getApplicationContext(), EditorActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_delete_all_entries:
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
